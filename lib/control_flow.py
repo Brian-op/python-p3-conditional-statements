75a459ee141940a819cb2148e7de960109705de0
Brian <brian.kiprono8@student.moringaschool.com>
2025-05-07 23:25:54 +0300

    weather

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
-
 def admin_login(username, password):
-    if (username == "admin" or username == "ADMIN") or password == 12345 :
-        return("Access granted")
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
     else:
-        return("Access denied")
-        
-
+        return "Access denied"
+ 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if temperature <= 40 :
+        return "It's brisk out there!" 
+    elif 40 <= temperature <= 65:
+        return  "It's a little chilly out there!"
+    elif temperature > 85 :
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
 
 def fizzbuzz(num):
     # your code here
